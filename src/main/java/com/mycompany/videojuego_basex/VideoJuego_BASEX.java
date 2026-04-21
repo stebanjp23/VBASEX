e0794b23dc2ac3283197ff3b5310adfbf95b6053
stebanjp23 <juhe741@vidalibarraquer.net>
2026-04-21 09:09:28 +0200

    first

---
+++ src/main/java/com/mycompany/videojuego_basex/VideoJuego_BASEX.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.videojuego_basex;
+
+import com.mycompany.videojuego_basex.db.ConexiónbaseX;
+
+/**
+ *
+ * @author juane
+ */
+public class VideoJuego_BASEX {
+
+    public static void main(String[] args) {
+        ConexiónbaseX.getContext();
+    }
+}
