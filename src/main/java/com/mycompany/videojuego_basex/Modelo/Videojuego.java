4f95469b8fcfb2877e819c5363d4a029f0855350
stebanjp23 <juhe741@vidalibarraquer.net>
2026-04-28 08:27:33 +0200

    modelo creado, dao tambien

---
+++ src/main/java/com/mycompany/videojuego_basex/Modelo/Videojuego.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.videojuego_basex.Modelo;
+
+/**
+ *
+ * @author juane
+ */
+public class Videojuego {
+    private String id;
+    private String estado;
+    private String titulo;
+    private String desarrollador;
+    private double precio;
+    private String[] plataforma;
+    private String año;
+
+    public Videojuego() {
+    }
+
+    public Videojuego(String id, String estado, String titulo, String desarrollador, double precio, String[] plataforma, String año) {
+        this.id = id;
+        this.estado = estado;
+        this.titulo = titulo;
+        this.desarrollador = desarrollador;
+        this.precio = precio;
+        this.plataforma = plataforma;
+        this.año = año;
+    }
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesarrollador() {
+        return desarrollador;
+    }
+
+    public void setDesarrollador(String desarrollador) {
+        this.desarrollador = desarrollador;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String[] getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String[] plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getAño() {
+        return año;
+    }
+
+    public void setAño(String año) {
+        this.año = año;
+    }
+    
+    
+    
+    
+}
--- src/main/java/com/mycompany/videojuego_basex/db/ConexionBaseX.java
+++ src/main/java/com/mycompany/videojuego_basex/db/ConexionBaseX.java
@@ -12,33 +12,35 @@ import org.basex.core.StaticOptions;
 
 public final class ConexionBaseX {
 
-    private static final String DB_NAME = "VideojocsDB";
-    private static final String RESOURCE_NAME = "/catalogo.xml";
-    private static final Path DB_PATH = Paths.get("target", "basex-data").toAbsolutePath();
+    private static final String DB_NAME = "VideojocsDB"; // nombre de la base de datos
+    private static final String RESOURCE_NAME = "/catalogo.xml"; // Ruta del archivo XML
+    private static final Path DB_PATH = Paths.get("target", "basex-data").toAbsolutePath(); //Ruta donde se almacenara la BD y sus archivos
 
-    private static Context context;
+    private static Context context; //Creación del contexto (funciona como si fuera una sesión para interactuar con el BASEX)
 
-    private ConexionBaseX() {
+    private ConexionBaseX() { //Constructor vacio 
     }
 
-    public static synchronized void init() throws IOException {
+    public static synchronized void init() throws IOException { // 
         if (context != null) {
             return;
         }
 
-        Files.createDirectories(DB_PATH);
+        Files.createDirectories(DB_PATH); //Creación de fichero donde se alamcenara los archivos de la BD
 
+        // Configuración de BaseX 
         StaticOptions options = new StaticOptions(false);
         options.set(StaticOptions.DBPATH, DB_PATH.toString());
         context = new Context(options);
 
-        try (LocalSession session = new LocalSession(context)) {
-            if (!context.databases.list().contains(DB_NAME)) {
-                try (InputStream input = ConexionBaseX.class.getResourceAsStream(RESOURCE_NAME)) {
+        // Creación de la BD
+        try (LocalSession session = new LocalSession(context)) { //Abrimos la sesión con BaseX
+            if (!context.databases.list().contains(DB_NAME)) { //Comprueba si la base de datos existe
+                try (InputStream input = ConexionBaseX.class.getResourceAsStream(RESOURCE_NAME)) { 
                     if (input == null) {
                         throw new IOException("No se ha encontrado el recurso " + RESOURCE_NAME);
                     }
-                    session.create(DB_NAME, input);
+                    session.create(DB_NAME, input); //Crea la BD con los datos del XML 
                 }
             }
         }
